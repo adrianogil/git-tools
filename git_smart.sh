351cb3c6f5ba5eae0eb02111222ab8c84eeba508
Adriano Gil <adrianogil.san@gmail.com>
2018-07-21 10:29:09 -0400

    Added support for shell projects in smart git add

--- git_smart.sh
+++ git_smart.sh
@@ -5,6 +5,9 @@ function gsmart-add()
     if [ "$(ls *.py 2> /dev/null | wc -l)" -gt "$min_py_proj_dir" ]; then
         echo 'Python project identified'
         git add *.py
+    elif [ "$(ls *.sh 2> /dev/null | wc -l)" -gt "$min_py_proj_dir" ]; then
+        echo 'Shell project identified'
+        git add *.sh
     else
         echo 'Unknown project'
     fi

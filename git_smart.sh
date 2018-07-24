be059e6e476c2052178272c848f4f9a00065d12d
Adriano Gil <adrianogil.san@gmail.com>
2018-07-23 21:24:06 -0400

    Added new project type to smart add

--- git_smart.sh
+++ git_smart.sh
@@ -8,6 +8,9 @@ function gsmart-add()
     elif [ "$(ls *.sh 2> /dev/null | wc -l)" -gt "$min_py_proj_dir" ]; then
         echo 'Shell project identified'
         git add *.sh
+    elif [ "$(ls *.tex 2> /dev/null | wc -l)" -gt "$min_py_proj_dir" ]; then
+        echo 'LaTeX project identified'
+        git add *.tex *.bib
     else
         echo 'Unknown project'
     fi

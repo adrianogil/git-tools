55d0c7a734f9ba45cf2f7a318a8f894f1e061522
Adriano Gil <adrianogil.san@gmail.com>
2018-08-15 20:33:37 -0400

    Added option to show date of first and last commits when counting commits

--- python/gcount_branch.py
+++ python/gcount_branch.py
@@ -2,6 +2,7 @@ import sys, subprocess
 
 import utils
 
+
 def count_commits_in_branch(args=[], extra_args=[]):
     branches = subprocess.check_output("git branch -r ", shell=True)
     branches = branches.decode("utf8").strip().split('\n')
@@ -18,7 +19,25 @@ def count_commits_in_branch(args=[], extra_args=[]):
             total_commits = subprocess.check_output("git log --oneline --graph " + b + " | wc -l", shell=True)
             total_commits = total_commits.decode("utf8").strip()
 
-            print(b + ": " + total_commits)
+            extra_info = ""
+
+            if '-t' in extra_args:
+                first_commit = subprocess.check_output("git rev-list --max-parents=0 HEAD --abbrev-commit", shell=True)
+                first_commit = first_commit.decode("utf8").strip()
+
+                last_commit = subprocess.check_output("git rev-list HEAD --abbrev-commit -1", shell=True)
+                last_commit = last_commit.decode("utf8").strip()
+
+                first_commit_date = subprocess.check_output("git log -1 --format=\%ai " + first_commit, shell=True)
+                first_commit_date = first_commit_date.decode("utf8").strip()
+
+                last_commit_date = subprocess.check_output("git log -1 --format=\%ai " + last_commit, shell=True)
+                last_commit_date = last_commit_date.decode("utf8").strip()
+
+                extra_info = '[' + first_commit_date + ' - ' + last_commit_date + ']' 
+
+
+            print(b + ": " + total_commits + ' commits ' + extra_info)
         except:
             pass
 
@@ -117,10 +136,16 @@ def parse_commands(args):
     if args is None:
         return
 
+    parse_count = 0
+
     # print('DEBUG: Parsing args: ' + str(args))
     for a in args:
         if a in commands_parse:
             commands_parse[a](args[a], args)
+            parse_count = parse_count + 1
+
+    if parse_count == 0:
+        count_commits_in_branch([], args)
 
 args = parse_arguments()
 parse_commands(args)

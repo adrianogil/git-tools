4dec70bfe6ee3de73088079790cb97b23c2200f8
Adriano Gil <adrianogil.san@gmail.com>
2019-09-23 20:58:02 -0400

    Added functions as gtool

--- git_repos.sh
+++ git_repos.sh
@@ -1,17 +1,27 @@
+# gtool gt-repos
 function gfrepos()
 {
     f '.git' | xa echo {} | rev | cut -c6- | rev
 }
 alias gt-repos="gfrepos"
 
-function gfrepos-urls()
+# gtool gfrepos-urls
+function gt-repos-urls-current-folder()
 {
     gfrepos | xa cat {}/.git/config | grep "url = " | cut -c8-
 }
-alias gt-repos-urls="gfrepos-urls"
 
-function gurls()
+# gtool gt-repos-current-urls
+function gt-repos-urls()
 {
     cat $(git rev-parse --show-toplevel)/.git/config  | grep "url = " | cut -c8-
 }
-alias gt-repos-current-urls="gurls"
+alias gurls="gt-repos-urls"
+
+# gtool gt-repos-go-to-github
+function gt-repos-go-to-github()
+{
+    url_path=$(gt-repos-urls-current-folder | head -1)
+    github_url="http://www."$(echo ${url_path}| cut -c5- | tr ":" "/")
+    o ${github_url}
+}

da189e215bd695aa784168b47c8f6f7be80a1ddd
Adriano Gil <adrianogil.san@gmail.com>
2019-09-24 08:08:41 -0400

    Added a function to git_tools python API

--- python/git_tools/git_tools.py
+++ python/git_tools/git_tools.py
@@ -11,3 +11,14 @@ def get_commit_date(repo_dir, ref):
     commit_date = datetime.strptime(get_commit_date_output[:-9], "%Y-%m-%d %H:%M")
 
     return commit_date
+
+
+def get_commits_with(file=""):
+    get_hashes_cmd = "git log --pretty=format:'%%h' %s" % (file,)
+    get_hashes_output = subprocess.check_output(get_hashes_cmd, shell=True)
+    get_hashes_output = get_hashes_output.strip()
+    get_hashes_output = get_hashes_output.decode("utf8")
+
+    hash_list = get_hashes_output.split("\n")
+
+    return hash_list

315a8a07876374beeef4b0abb2d6e5876540fbc2
Adriano Gil <adrianogil.san@gmail.com>
2020-01-23 13:26:48 -0400

    Added function to ease commit
    - Added gt-commit as a gtool

--- bashrc.sh
+++ bashrc.sh
@@ -3,6 +3,7 @@ source ${GIT_TOOLS_DIR}/git_smart.sh
 source ${GIT_TOOLS_DIR}/personal_aliases.sh
 source ${GIT_TOOLS_DIR}/git_repos.sh
 source ${GIT_TOOLS_DIR}/git_stage.sh
+source ${GIT_TOOLS_DIR}/git_commit.sh
 source ${GIT_TOOLS_DIR}/git_internals.sh
 source ${GIT_TOOLS_DIR}/git_attributes.sh
 source ${GIT_TOOLS_DIR}/git_history.sh
---
+++ git_commit.sh
@@ -0,0 +1,6 @@
+# gtool gt-commit
+function gt-commit()
+{
+    # Commit as Adriano Gil (personal email)
+    git commit --author='Adriano Gil <adrianogil.san@gmail.com>'
+}

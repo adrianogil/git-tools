c6db1e12086ba095dc1d932c4660c1930f6923b9
Adriano Gil <adrianogil.san@gmail.com>
2020-01-25 09:27:23 -0400

    Added tool to move between local branches

--- git_navigation.sh
+++ git_navigation.sh
@@ -1,8 +1,10 @@
-function ggo()
+
+# gtool gt-navigate-to-local-branch
+function gt-navigate-to-local-branch()
 {
     target_branch=$(git branch -a | cut -c3- | sk)
     echo "Let's checkout to branch: "$target_branch
     git checkout ${target_branch}
 }
-alias gt-nav-go="ggo"
+alias gt-nav-go="gt-navigate-to-local-branch"
 

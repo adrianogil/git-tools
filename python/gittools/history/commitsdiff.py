aa943515841a792aa1e7c4bf07e234fc20fa7811
Adriano Gil <adrianogil.san@gmail.com>
2021-09-02 16:20:45 -0400

    Minor improvement on gh-diff command

--- python/gittools/history/commitsdiff.py
+++ python/gittools/history/commitsdiff.py
@@ -5,15 +5,22 @@ def get_commits_diff(ref1, ref2):
 	hashes_ref1 = get_hash_log([ref1])
 	hashes_ref2 = get_hash_log([ref2])
 
-	print("Missing commits from " + str(ref1))
+	diff_hash1 = []
 	for hash1 in hashes_ref1:
 		if hash1 not in hashes_ref2:
-			print(hash1)
+			diff_hash1.append(hash1)
+	
+	print("Missing commits from " + str(ref1) + " (%s commits)" % (len(diff_hash1)))
+	for hash1 in diff_hash1:
+		print(hash1)
 
-	print("Missing commits from " + str(ref2))
+	diff_hash2 = []
 	for hash2 in hashes_ref2:
 		if hash2 not in hashes_ref1:
-			print(hash2)
+			diff_hash2.append(hash2)
+	print("Missing commits from " + str(ref1) + " (%s commits)" % (len(diff_hash2)))
+	for hash2 in diff_hash2:
+		print(hash2)
 
 
 

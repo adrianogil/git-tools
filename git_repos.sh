0e1f2568dcc53d1dca1d158149ef17dc1012a51e
Adriano Gil <adrianogil.san@gmail.com>
2022-02-26 10:43:51 -0400

    Improved repos commands

--- git_repos.sh
+++ git_repos.sh
@@ -1,27 +1,27 @@
-# gtool gt-repos
+# gtool gt-repos: Find repos
 function gfrepos()
 {
     f '.git' | xa echo {} | rev | cut -c6- | rev
 }
 alias gt-repos="gfrepos"
 
-# gtool gt-repos-urls-current-folder
+
 function gt-repos-urls-current-folder()
 {
     gfrepos | xa cat {}/.git/config | grep "url = " | cut -c8-
 }
 
-# gtool gt-repos-urls
+# gtool gt-repos-urls: List repo urls
 function gt-repos-urls()
 {
     cat $(git rev-parse --show-toplevel)/.git/config  | grep "url = " | cut -c8-
 }
 alias gurls="gt-repos-urls"
 
-# gtool gt-repos-go-to-github
-function gt-repos-go-to-github()
+# gtool gt-repos-open-site: Open repo site 
+function gt-repos-open-site()
 {
-    url_path=$(gt-repos-urls-current-folder | head -1)
-    github_url="http://www."$(echo ${url_path}| cut -c5- | tr ":" "/")
-    o ${github_url}
+    url_path=$(gt-repos-urls | head -1)
+    repo_url="http://www."$(echo ${url_path}| cut -c5- | rev | cut -c5- | rev | tr ":" "/")
+    o ${repo_url}
 }

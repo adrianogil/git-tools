93a5bbae64d7fbbcc2b8288f831b55ab05ef0482
Adriano Gil <adrianogil.san@gmail.com>
2024-04-08 21:40:46 -0400

    fix: solve issue with gpick command which was skipping the first input
    - The iteration was skipping the first element, so just fixed the initial
    value of the sequence
    - Added more messages

--- python/gittools/pick.py
+++ python/gittools/pick.py
@@ -15,28 +15,30 @@ def is_int(s):
     return False
 
 if len(input_numbers) == 1 and " " in input_numbers[0].strip():
-    input_numbers = input_numbers.split(" ")
+    input_numbers = input_numbers[0].strip().split(" ")
 
 commits_order = []
 
 max_commit_backtrack = 0
 
-for i in range(1, len(input_numbers)):
+for i in range(0, len(input_numbers)):
     if is_int(input_numbers[i]):
         commits_order_number = int(input_numbers[i])
+        # print(commits_order_number)
         if commits_order_number > max_commit_backtrack:
             max_commit_backtrack = commits_order_number
 
         # Get ref for each commit
-        commit = clitools.run_cmd("git rev-parse --short HEAD~" + sys.argv[i])
+        commit = clitools.run_cmd(f"git rev-parse --short HEAD~{commits_order_number}")
         commits_order.append(commit)
-        print('debug: ' + commit)
+        print(f'Found commit {commits_order_number}: {commit}')
 
 max_commit_backtrack = max_commit_backtrack + 1
 clitools.run_cmd("git reset --hard HEAD~" + str(max_commit_backtrack))
 
-print('max_commit_backtrack: ' + str(max_commit_backtrack))
+print(f'Got back {max_commit_backtrack} commits')
 
 # Apply each ref in the correct order
 for c in reversed(commits_order):
+    print(f"Applying commit {c}")
     clitools.run_cmd("git cherry-pick " + c)

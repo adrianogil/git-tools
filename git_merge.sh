641ee6f9aef62fdac38640a9df798b91e51ab8f0
Adriano Gil <adrianogil.san@gmail.com>
2024-05-25 09:33:11 -0400

    feat: added gt-merge command
    - added gt-merge function to allow select a branch to merge using
    default-fuzzy-finder
    - renamed gt-merge-branch to gt-merge-local-branch

--- git_merge.sh
+++ git_merge.sh
@@ -12,6 +12,7 @@ function gt-rebase()
 }
 alias gb-fz="gt-rebase"
 
+# gtool gt-rebase-local-branch: rebase from a local branch
 function gt-rebase-local-branch()
 {
     target_branch=$(git branch -a | cut -c3- | sed 's/origin//g' | cut -c2- | default-fuzzy-finder)
@@ -19,7 +20,16 @@ function gt-rebase-local-branch()
     git rebase ${target_branch}
 }
 
-function gt-merge-branch()
+# gtool gt-merge: merge from a remote branch
+function gt-merge()
+{
+    target_branch=$(gbk)
+    echo "Let's merge branch: "$target_branch
+    git merge ${target_branch}
+}
+
+# gtool gt-merge-local-branch: merge from a local branch
+function gt-merge-local-branch()
 {
     target_branch=$(git branch -a | cut -c3- | default-fuzzy-finder)
     echo "Let's merge branch: "$target_branch

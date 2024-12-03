821c157d67e3f95ec2e35f4d1eeb2fa8a2983fb2
Adriano Gil <adrianogil.san@gmail.com>
2024-12-02 20:43:34 -0400

    feat: added a new command to pick a modified file

--- git_status.sh
+++ git_status.sh
@@ -5,3 +5,11 @@ function gt-list-untracked-files()
 {
     git ls-files --others --exclude-standard
 }
+alias gfiles-untracked="gt-list-untracked-files"
+
+# gtool gt-list-modified-files: list modified files
+function gt-list-modified-files()
+{
+    git ls-files --modified
+}
+alias gfiles-modified="gt-list-modified-files"
